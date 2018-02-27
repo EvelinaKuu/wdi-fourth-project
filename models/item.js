81b983645abf4a62f8966384aa83673f786d75c6
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-02-27 11:29:14 +0000

    updating item schema

--- models/item.js
+++ models/item.js
@@ -1,11 +1,32 @@
 const mongoose = require('mongoose');
 
+const commentSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  createdBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+}, {
+  timestamps: true
+});
+commentSchema.methods.belongsTo = function commentBelongsTo(user) {
+  return this.createdBy.id === user.id;
+};
+
 const itemSchema = mongoose.Schema({
   title: { type: String, required: true },
   image: { type: String, required: true },
+  price: { type: Number, required: true },
   category: { type: String, required: true },
-  description: { type: String, required: true }
+  description: { type: String, required: true },
+  soldBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  comments: [ commentSchema]
 });
+
+itemSchema.methods.belongsTo = function belongsTo(user) {
+  // check if the user createed the item is the same as the person who is logged in
+  // 'this' is the instance of the item that we are calling the 'belongsTo' method on
+  // user is the user object that we will pass this method /the user who is logged in
+  return this.soldBy.id === user.id;
+
+};
 
 itemSchema.set('toJSON', {
   getters: true,
--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
