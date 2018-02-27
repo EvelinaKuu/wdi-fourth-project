264007fa138bb7b97093a03ab048b46f56f21090
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-02-27 11:32:51 +0000

    updating user schema

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   firstname: { type: String, required: true },
   lastname: { type: String, required: true },
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
