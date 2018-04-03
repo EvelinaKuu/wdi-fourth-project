71588705479055298551c5054941520dfc742e1f
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-04-03 14:26:16 +0100

    adding new hero image

--- src/components/items/ItemsShow.js
+++ src/components/items/ItemsShow.js
@@ -17,7 +17,7 @@ class ItemsShow extends React.Component {
   componentDidMount() {
     Axios
       .get(`/api/items/${this.props.match.params.id}`)
-      .then(res => this.setState({ item: res.data }, () => console.log(this.state)))
+      .then(res => this.setState({ item: res.data }))
       .catch(err => console.log(err));
   }
 
@@ -42,106 +42,91 @@ class ItemsShow extends React.Component {
       .catch(err => console.log(err));
   }
 
-  handleChange = ({ target: { value }}) => {
-    this.setState({ newComment: { content: value }}, () => console.log(this.state.newComment));
-  }
+    handleChange = ({ target: { value }}) => {
+      this.setState({ newComment: { content: value }}, () => console.log(this.state.newComment));
+    }
 
-  handleSubmit = e => {
-    // console.log('handleSubmit works');
-    e.preventDefault();
-    Axios
-      .post(`/api/items/${this.state.item.id}/comments`, this.state.newComment,
-        {
-          headers: { 'Authorization': `Bearer ${Auth.getToken()}`}
+    handleSubmit = e => {
+      // console.log('handleSubmit works');
+      e.preventDefault();
+      Axios
+        .post(`/api/items/${this.state.item.id}/comments`, this.state.newComment,
+          {
+            headers: { 'Authorization': `Bearer ${Auth.getToken()}`}
+          })
+        .then((res) => {
+          this.setState(prevState => {
+            const newState = prevState;
+            newState.item.comments.push(res.data);
+            newState.newComment.content = '';
+            return newState;
+          });
         })
-      .then((res) => {
-        this.setState(prevState => {
-          const newState = prevState;
-          newState.item.comments.push(res.data);
-          newState.newComment.content = '';
-          return newState;
-        });
-      })
-      .catch(err => this.setState({ errors: err.response.data.errors }));
-  }
+        .catch(err => this.setState({ errors: err.response.data.errors }));
+    }
 
-  deleteComment = (id) => {
-    Axios
-      .delete(`/api/items/${this.state.item.id}/comments/${id}`,
-        {
-          headers: { 'Authorization': `Bearer ${Auth.getToken()}`}
-        })
-      .then((res) => this.setState({ item: res.data}))
-      .catch(err => console.log(err));
-  }
-  render() {
-    console.log(this.state.item.likes);
-    return (
-      <div className="columns">
-        <div className="column is-quarter">
-          <img src={this.state.item.image} className="item-display" />
-          <p>Likes: { this.state.item.likes && this.state.item.likes.length } </p>
-          <div className="field is-grouped">
-            <BackButton history={this.props.history} />
-
-            { Auth.isAuthenticated() && <Link to={`/items/${this.state.item.id}/edit`} className="button is-white">
-              <i className="fas fa-pencil" aria-hidden="true"></i>Edit
-            </Link> }
-
-            {/* <i class="fas fa-edit"></i> */}
-            {' '}
-
-            { Auth.isAuthenticated() && <button className="button is-white" onClick={this.deleteItem}>
-              <i className="fa fa-trash" aria-hidden="true"></i>Delete
-            </button> }
-
-
-
-
-
-            { Auth.isAuthenticated() &&
+      deleteComment = (id) => {
+        Axios
+          .delete(`/api/items/${this.state.item.id}/comments/${id}`,
+            {
+              headers: { 'Authorization': `Bearer ${Auth.getToken()}`}
+            })
+          .then((res) => this.setState({ item: res.data}))
+          .catch(err => console.log(err));
+      }
+      render() {
+        console.log(this.state.item.likes);
+        return (
+          <div className="columns">
+            <div className="column is-quarter">
+              <img src={this.state.item.image} className="item-display" />
+              <p>Likes: { this.state.item.likes && this.state.item.likes.length } </p>
+              <div className="field is-grouped">
+                <BackButton history={this.props.history} />
+                { Auth.isAuthenticated() && <Link to={`/items/${this.state.item.id}/edit`} className="button is-white">
+                  <i className="fas fa-pencil" aria-hidden="true"></i>Edit
+                </Link> }
+                {' '}
+                { Auth.isAuthenticated() && <button className="button is-white" onClick={this.deleteItem}>
+                  <i className="fa fa-trash" aria-hidden="true"></i>Delete
+                </button> }
+                { Auth.isAuthenticated() &&
               this.state.item.likes &&
               this.state.item.likes.every(like => like !== Auth.getPayload().userId) &&
               <button className="button is-white" onClick={this.likeItem} >
                 <i className="fas fa-heart"></i>
               </button>
-            }
-
-          </div>
-        </div>
-
-        <div className="column">
-          <h3>{this.state.item.title}</h3>
-          <h3>Price: £{this.state.item.price}</h3>
-          <h3>Category: {this.state.item.category}</h3>
-          <h3>Description of the item: {this.state.item.description}</h3>
-          {this.state.item.createdBy && <h4>Sold by: <Link to={`/users/${this.state.item.createdBy.id}`}>{this.state.item.createdBy.username}</Link></h4>}
-          {this.state.item.createdBy && <p><a href={`mailto:${this.state.item.createdBy.email}`}>Send email to seller</a></p>}
-          <h3>Comments:</h3>
-          {this.state.item.comments && this.state.item.comments.map(comment => {
-            return(
-              <div key={comment._id} >
-                <p>{comment.content} </p>
-                <p>{comment.createdBy.username} </p>
-
-                <button className="button is-white" onClick={() => this.deleteComment(comment._id)}>
-                  <i className="fa fa-trash" aria-hidden="true"></i>
-                </button>
-
+                }
               </div>
-
-            );
-          })}
-
-          <CommentForm
-            handleSubmit={this.handleSubmit}
-            handleChange={this.handleChange}
-            newComment={this.state.newComment}
-          />
-        </div>
-      </div>
-    );
-  }
+            </div>
+            <div className="column">
+              <h3>{this.state.item.title}</h3>
+              <h3>Price: £{this.state.item.price}</h3>
+              <h3>Category: {this.state.item.category}</h3>
+              <h3>Description of the item: {this.state.item.description}</h3>
+              {this.state.item.createdBy && <h4>Sold by: <Link to={`/users/${this.state.item.createdBy.id}`}>{this.state.item.createdBy.username}</Link></h4>}
+              {this.state.item.createdBy && <p><a href={`mailto:${this.state.item.createdBy.email}`}>Send email to seller</a></p>}
+              <h3>Comments:</h3>
+              {this.state.item.comments && this.state.item.comments.map(comment => {
+                return(
+                  <div key={comment._id} >
+                    <p>{comment.content} </p>
+                    <p>{comment.createdBy.username} </p>
+                    <button className="button is-white" onClick={() => this.deleteComment(comment._id)}>
+                      <i className="fa fa-trash" aria-hidden="true"></i>
+                    </button>
+                  </div>
+                );
+              })}
+              <CommentForm
+                handleSubmit={this.handleSubmit}
+                handleChange={this.handleChange}
+                newComment={this.state.newComment}
+              />
+            </div>
+          </div>
+        );
+      }
 }
 
 export default ItemsShow;
